33ffa8f4d8f55effe3a52df003c09827c22e9591
saurabh sinha <sinha.saurabh06@gmail.com>
2024-10-14 15:44:54 +0000

    intial checkin

---
+++ c3-s3bucket.tf
@@ -0,0 +1,18 @@
+/*
+# Call our Custom Terraform Module which we built earlier
+module "website_s3_bucket123" {
+  source  = "app.terraform.io/hcta-demo1/s3-website/aws"
+  version = "1.0.1"
+  # insert required variables here
+  bucket_name = var.my_s3_bucket
+  tags = var.my_s3_tags
+}
+*/
+
+module "website_s3_bucket" {
+  source  = "app.terraform.io/dcn-iac/s3-website/aws"
+  version = "1.0.1"
+  # insert required variables here
+  bucket_name = var.my_s3_bucket
+  tags = var.my_s3_tags
+}
